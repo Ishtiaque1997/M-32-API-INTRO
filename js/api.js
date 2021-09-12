ad213052715af0b39e6f99fd502cb69045db2dbf
Ishtiaque1997 <ishtiaquea695@gmail.com>
2021-09-12 07:03:42 +0600

    more api

--- js/api.js
+++ js/api.js
@@ -6,3 +6,35 @@ function loadData(){
  .then(res=>res.json())
  .then(data=>console.log(data))
 }
+function loadUsers(){
+ fetch('https://jsonplaceholder.typicode.com/users')
+ .then(res=>res.json())
+ .then(data=>displayUsers(data))
+
+}
+
+function loadPost(){
+ fetch('https://jsonplaceholder.typicode.com/posts')
+ .then(res=>res.json())
+ .then(data=> showPosts(data));
+}
+function displayUsers(users){
+ const ul=document.getElementById('users');
+  for (const user of users){
+   // console.log(user.name)
+   const li=document.createElement('li');
+   li.innerText=`name:${user.name},email:${user.email}`;
+   ul.appendChild(li)
+  }
+}
+
+ function showPosts(data){
+  const lists=document.getElementById('post-title')
+  for(const post of data)
+   {
+    const list=document.createElement('li');
+    list.innerText=`title:${post.title}`;
+    lists.appendChild(list)
+
+   }
+ }
